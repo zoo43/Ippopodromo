6d5c1bc48c012dddd9c1076d4789b4697a5a1a30
cecio999 <58601180+cecio999@users.noreply.github.com>
2021-01-13 21:20:15 +0100

    Aggiunti controlli a password e mail
    Aggiunto numero minimo di caratteri per Password.
    Aggiunto filtro per mail (da testare).

--- php/Autenticazione/register.php
+++ php/Autenticazione/register.php
@@ -20,7 +20,7 @@ session_start();
     <form method="post" onsubmit="return validazioneInput()" action="checkRegister.php">
         <h1>Registrazione</h1>
         <input type="text" id="username" placeholder="Nome Utente" name="username" maxlength="50" required>
-        <input type="password" id="password" placeholder="Password" name="password" required>
+        <input type="password" id="password" placeholder="Password" name="password" minlength="6" required>
         <input type="text" id="name" placeholder="Nome" name="name" maxlength="50" required>
         <input type="text" id="surname" placeholder="Cognome" name="surname" maxlength="50" required>
         
@@ -28,7 +28,7 @@ session_start();
         
         <input type="text" id="address" placeholder="Indirizzo" name="address" maxlength="50" required>
         <input type="text" id="city" placeholder="Città" name="city" maxlength="50" required>
-        <input type="text" id="mail" placeholder="Mail" name="mail" maxlength="50" required>
+        <input type="text" id="mail" placeholder="Mail" name="mail" pattern="[a-zA-Z0-9]+[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]{0,}[a-zA-Z0-9]{0,}@[a-zA-Z0-9]{3,}\.[a-zA-Z]{2,3}$" maxlength="50" required>
 
         <button type="submit" name="register">Registrati</button>
         <?php        
