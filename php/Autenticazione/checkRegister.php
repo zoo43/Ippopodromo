89dab1179d98643f8f5a42253c639c9cc1fe5c30
Matteo <matteo16.martini@gmail.com>
2021-01-13 10:54:57 +0100

    Update checkRegister.php
    Ora da Registrato sei anche logginato in automatico

--- php/Autenticazione/checkRegister.php
+++ php/Autenticazione/checkRegister.php
@@ -6,13 +6,16 @@ session_Start();
 if(isset($_POST['register'])){
     $dbAccess = new DBAccess();
     $conn = $dbAccess->openDBConnection();
-    if($dbAccess->verificaPresenza($_POST['username'], $_POST['mail'])){
+    $username = $_POST['username'];
+    if($dbAccess->verificaPresenza($username, $_POST['mail'])){
         $_SESSION["error"] = 'Nome Utente o mail già occupati';
         header("location:register.php");
     }
     else{
-        $dbAccess->inserisciUtente($_POST['username'],$_POST['password'], $_POST['name'], $_POST['surname'], $_POST['date'], $_POST['address'], $_POST['city'], $_POST['mail']);
         echo "Sei registrato!";
+        $dbAccess->inserisciUtente($username,$_POST['password'], $_POST['name'], $_POST['surname'], $_POST['date'], $_POST['address'], $_POST['city'], $_POST['mail']);
+        $_SESSION["username"] = $username;
+        $_SESSION["credito"] = "100";
     }
     $dbAccess->closeDBConnection();
 }
