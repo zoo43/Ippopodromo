8611b918ee3153f101f8153baed83c51cdf572e6
Dam-99 <57217353+Dam-99@users.noreply.github.com>
2021-01-22 16:12:45 +0100

    Risolto errrore pannello admin

--- php/auth.php
+++ php/auth.php
@@ -8,9 +8,6 @@ function areaAutenticazione($pagina, $index = false){
         {
             $admin = "<li><a href='../Admin/admin.php'>Pannello amministratore</a></li>";
         }
-        else if($index) {
-            $admin = "<li><a href='php/Admin/admin.php'>Pannello amministratore</a></li>";
-        }
         $autenticazione = "<li><a href='../Autenticazione/logout.php'>Logout</a></li>";
     }
     else{
