aabe6e4b61071ceb58c49a0b2efae4c2fe3bb940
Dam-99 <57217353+Dam-99@users.noreply.github.com>
2021-01-24 15:34:45 +0100

    Completato pannello admin (close #3)

--- php/Admin/aggiungiGara.php
+++ php/Admin/aggiungiGara.php
@@ -1,13 +1,7 @@
 <?php 
 require_once('../database.php');
 
-$risultatiAggiunta = '';
-$cavalli = '';
-$dbAccess = new DBAccess();
-$conn = $dbAccess->openDBConnection();
-
-
-function stampaListaCavalli($dbAccess,$cavalli)
+function listaCavalliToString($dbAccess, $cavalli)
 {
     $result = $dbAccess->getCavalli(true);
     while($row = mysqli_fetch_array($result))
@@ -20,44 +14,47 @@ function stampaListaCavalli($dbAccess,$cavalli)
     return $cavalli;
 }
 
+$cavalli = '';
+$risultatoAggiunta = '';
+$gare = '';
+$pagina = file_get_contents('../../html/admin/aggiungiGara.html');
+$dbAccess = new DBAccess();
+$conn = $dbAccess->openDBConnection();
+if($conn)
+{
+    $result = $dbAccess->getGare("0");
+    while($row = mysqli_fetch_array($result)) {
+        $gare .= '<tr><td>Gara' . $row['idGara'] . '</td><td>' . $row['dataGara'] . '</td><td><a href=inserisciRisultati?value="' . $row['idGara'] . '">Inserisci risultati</td></tr>';
+    }
+    $dbAccess->closeDBConnection();
+    mysqli_free_result($result);
+    
+    $pagina = str_replace("<lista-gare />", $gare, $pagina);
+} else {
+    printf("Si è verificato un errore di connessione. Si prega di attendere prima di riprovare.");
+}
+
 if(isset($_POST['register']))
 {
-    if($conn)
-    {
+    $conn = $dbAccess->openDBConnection();
+    if($conn) {
         if($dbAccess->caricaGare($_POST['date'], $_POST['time'], $_POST['cavalli']))
         {
-            $risultatiAggiunta = "<p class='inserimentoRiuscito'>Gara inserita con successo</p>";
-            $cavalli=stampaListaCavalli($dbAccess, $cavalli);
+            $risultatoAggiunta = "<p class='inserimentoRiuscito'>Gara inserita con successo</p>";
         }
         else
         {
-            $risultatoAggiunta .= "<p class='inserimentoFallito'>C'è stato un problema</p>";
+            $risultatoAggiunta = "<p class='inserimentoFallito'>C'è stato un problema con la creazione della gara</p>";
         }
         $dbAccess->closeDBConnection();
     }
-    else{
-        $risultatoAggiunta = "<p class='inserimentoFallito'>Problema di connessione al DB</p>";
+    else {
+        $risultatoAggiunta = "<p class='inserimentoFallito>Problema durante la connessione al database.";
     }
-}
-else
-{
-    if($conn)
-    {
-        $cavalli=stampaListaCavalli($dbAccess, $cavalli);
-        $dbAccess->closeDBConnection();
-    }
-    else
-    {
-        printf("Si è verificato un errore di connessione. Si prega di attendere prima di riprovare.");
-    }
-
+    
+    $pagina = str_replace("<risultati-inserimento />", $risultatoAggiunta, $pagina);
+    unset($_POST['register']);
 }
 
-$pagina = file_get_contents("../../html/admin/aggiungiGara.html");
-$pagina = str_replace(
-    array("<risultati-inserimento />", "<cavalli />"),
-    array($risultatiAggiunta, $cavalli), 
-    $pagina
-);
 echo $pagina;
 ?>
