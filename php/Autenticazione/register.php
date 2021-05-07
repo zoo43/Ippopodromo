1587bf634e1857200811d562210dc6c3d35b5850
cecio999 <58601180+cecio999@users.noreply.github.com>
2021-05-07 17:59:56 +0200

    controllo età php
    controllo età php per registrazione

--- php/Autenticazione/register.php
+++ php/Autenticazione/register.php
@@ -3,7 +3,16 @@ require_once('../database.php');
 require_once('../auth.php');
 
 
+
 if(isset($_POST['register'])){
+	$birthDate = $_POST['date'];
+	$birthDate = explode("-", $birthDate);
+	$age = (date("md", date("U", mktime(0, 0, 0, $birthDate[1], $birthDate[2], $birthDate[0]))) > date("md")
+    ? ((date("Y") - $birthDate[0]) - 1)
+    : (date("Y") - $birthDate[0]));
+	
+	if($age>=18)
+	{
     $dbAccess = new DBAccess();
     $conn = $dbAccess->openDBConnection();
     if($dbAccess->verificaPresenza($_POST['username'], $_POST['mail'])){
@@ -11,18 +20,23 @@ if(isset($_POST['register'])){
         header("location:register.php");
     }
     else{
-        $dbAccess->inserisciUtente($_POST['username'],$_POST['password'], $_POST['name'], $_POST['surname'], $_POST['date'], $_POST['address'], $_POST['city'], $_POST['mail']);
+        $dbAccess->inserisciUtente($_POST['username'],$_POST['password'], $_POST['name'], $_POST['surname'], $_POST['date'], $_POST['address'],$_POST['city'], $_POST['mail']);
         $_SESSION["username"] = $_POST['username'];
         $_SESSION["credito"] = "100";
         header("location:../../");
     }
     $dbAccess->closeDBConnection();
+	}
+	else{
+		$_SESSION["error"] = 'Devi essere maggiorenne per usufruire di questo servizio';
+		printf("Devi essere maggiorenne per usufruire di questo servizio");
+	}
 }
 else{      
         if(isset($_SESSION["error"])){
             $error = $_SESSION["error"];
             echo '<script language="javascript">';
-            echo "alert('Nome utente o mail già presenti nel database')";
+            echo "alert(".$error.")";
             echo '</script>';
             unset($_SESSION["error"]);
         }
