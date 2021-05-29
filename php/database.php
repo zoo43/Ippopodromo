76e7fe4e93fcbc16c9c2fe57c3f3adb1e8e143d9
Dam-99 <57217353+Dam-99@users.noreply.github.com>
2021-05-30 00:38:03 +0200

    Contenuto pagina "chi siamo"
    - Corretto errore di battitura nel footer
    - Aggiunto ; alla fine delle query sul db

--- php/database.php
+++ php/database.php
@@ -29,7 +29,7 @@
 		//Registrazione
 		public function verificaPresenza($username, $mail)
 		{
-			$query = "SELECT * from Utente where nomeUtente='$username' or mail='$mail'";
+			$query = "SELECT * from Utente where nomeUtente='$username' or mail='$mail';";
 			
 			$result = mysqli_query($this->connection, $query);
 			if(mysqli_num_rows($result)>0){
@@ -44,7 +44,7 @@
 		public function inserisciUtente($username, $password, $name, $surname, $date, $address, $city, $mail)
 		{
 			$query = "INSERT INTO Utente (nomeUtente, nome, cognome, dataNascita, indirizzo, citta, credito, password, mail, admin) VALUES
-			('$username', '$name', '$surname', '$date', '$address', '$city', '100', '$password', '$mail', '0')";
+			('$username', '$name', '$surname', '$date', '$address', '$city', '100', '$password', '$mail', '0');";
 			
 			mysqli_query($this->connection, $query);
 			
@@ -60,7 +60,7 @@
 		
 		public function autentica($nome, $password)
 		{
-			$query = "SELECT * from Utente where nomeUtente='$nome' and BINARY password='$password'";
+			$query = "SELECT * from Utente where nomeUtente='$nome' and BINARY password='$password';";
 			
 			$result = mysqli_query($this->connection, $query);
 			
@@ -95,7 +95,7 @@
 			{
 				$query = "SELECT cavallo.idCavallo, descrizione, posizione, dataGara, nome, immagine, fiducia, stanchezza, velocita FROM (cavallo INNER JOIN partecipante ON cavallo.idCavallo = partecipante.idCavallo)
 				INNER JOIN gara ON gara.idGara=partecipante.idGara
-				WHERE cavallo.idCavallo = '$id' AND gara.stato=2";
+				WHERE cavallo.idCavallo = '$id' AND gara.stato=2;";
 				$result = mysqli_query($this->connection, $query);
 				if(mysqli_affected_rows($this->connection)>0)
 				{
@@ -106,7 +106,7 @@
 			}
 			else
 			{
-				$query = "SELECT idCavallo, descrizione, nome, immagine, velocita, fiducia FROM cavallo WHERE cavallo.idCavallo = '$id'";
+				$query = "SELECT idCavallo, descrizione, nome, immagine, velocita, fiducia FROM cavallo WHERE cavallo.idCavallo = '$id';";
 				$result = mysqli_query($this->connection, $query);
 				return $result;
 			}
@@ -116,14 +116,14 @@
 		//Risultati
 		public function getGare($stato)
 		{
-            $query = "SELECT dataGara, idGara from Gara where stato=$stato";
+            $query = "SELECT dataGara, idGara from Gara where stato=$stato;";
             $result = mysqli_query($this->connection, $query);
             return $result;
 		}
 		
 		public function getInfoGara($id)
 		{
-			$query = "SELECT dataGara, nome, cavallo.idCavallo, posizione, stato from gara inner join partecipante on gara.idGara=partecipante.idGara inner join cavallo on cavallo.idCavallo=partecipante.idCavallo where partecipante.idGara=$id order by posizione";
+			$query = "SELECT dataGara, nome, cavallo.idCavallo, posizione, stato from gara inner join partecipante on gara.idGara=partecipante.idGara inner join cavallo on cavallo.idCavallo=partecipante.idCavallo where partecipante.idGara=$id order by posizione;";
 			$result = mysqli_query($this->connection, $query);
 			return $result;
 		}
@@ -132,12 +132,12 @@
 		//Admin
 		public function caricaGare($date,$time,$arr)
 		{
-			$query = "INSERT INTO gara (dataGara,stato) VALUES('$date $time', '0')";
+			$query = "INSERT INTO gara (dataGara,stato) VALUES('$date $time', '0');";
 			
 			mysqli_query($this->connection, $query);
 			
 			
-			$query = "SELECT max(idGara) from gara";
+			$query = "SELECT max(idGara) from gara;";
 			$result = mysqli_query($this->connection, $query);
 			$row = mysqli_fetch_array($result);
 			$id=$row[0];
@@ -146,7 +146,7 @@
 				for ($i = 0; $i < count($arr) ; $i++) {
 					
 					$string = $arr[$i];
-					$query = "INSERT INTO partecipante (idGara,idCavallo) VALUES('$id','" . $string . "')";
+					$query = "INSERT INTO partecipante (idGara,idCavallo) VALUES('$id','" . $string . "');";
 					mysqli_query($this->connection, $query);
 				}
 				mysqli_free_result($result);
@@ -160,7 +160,7 @@
 		
 		public function caricaCavalli($nome,$velocita,$descrizione, $img)
 		{
-			$query = "INSERT INTO cavallo (nome,velocita,descrizione,immagine, fiducia,stanchezza) VALUES('$nome', '$velocita', '$descrizione','$img',15,0)";
+			$query = "INSERT INTO cavallo (nome,velocita,descrizione,immagine, fiducia,stanchezza) VALUES('$nome', '$velocita', '$descrizione','$img',15,0);";
 			
 			mysqli_query($this->connection, $query);
 			if(mysqli_affected_rows($this->connection)>0){
@@ -173,7 +173,7 @@
 		
 		public function getCavalliGara($idGara)
 		{
-			$query = "SELECT cavallo.idCavallo, descrizione, nome from cavallo inner join partecipante on cavallo.idCavallo=partecipante.idCavallo where idGara=$idGara";
+			$query = "SELECT cavallo.idCavallo, descrizione, nome from cavallo inner join partecipante on cavallo.idCavallo=partecipante.idCavallo where idGara=$idGara;";
 			$result = mysqli_query($this->connection, $query);
 			return $result;
 		}
@@ -183,7 +183,7 @@
 			$bool = false;
 			for($i=0;$i<count($posizioni);$i++)
 			{
-				$query = "UPDATE partecipante SET posizione" . "=". $posizioni[$i]." WHERE idGara='$idGara' AND idCavallo='". $idCavalli[$i]['id'] ."'";
+				$query = "UPDATE partecipante SET posizione" . "=". $posizioni[$i]." WHERE idGara='$idGara' AND idCavallo='". $idCavalli[$i]['id'] ."';";
 				mysqli_query($this->connection, $query);
 				if(mysqli_affected_rows($this->connection)>0){
 					$bool=true;
@@ -193,24 +193,24 @@
 				}
 			}
 			
-			$query = "SELECT idCavallo FROM partecipante WHERE idGara='$idGara' AND posizione=1";
+			$query = "SELECT idCavallo FROM partecipante WHERE idGara='$idGara' AND posizione=1;";
 			$result = mysqli_query($this->connection, $query);
 			if(mysqli_num_rows($result)>0)
 			{
 				$idCavallo = mysqli_fetch_array($result)["idCavallo"];
 				
-				$query = "SELECT nomeUtente, puntata FROM scommessa WHERE stato='0' AND idGara='$idGara' AND idCavallo='$idCavallo'";
+				$query = "SELECT nomeUtente, puntata FROM scommessa WHERE stato='0' AND idGara='$idGara' AND idCavallo='$idCavallo';";
 				$result = mysqli_query($this->connection, $query);
 				
 				while($row=mysqli_fetch_array($result))
 				{
 					$creditToAdd = $row["puntata"]*2;
 					$username = $row['nomeUtente'];
-					$query = "UPDATE utente SET credito=credito+$creditToAdd WHERE nomeUtente='$username'";
+					$query = "UPDATE utente SET credito=credito+$creditToAdd WHERE nomeUtente='$username';";
 					mysqli_query($this->connection, $query);
 					if(mysqli_affected_rows($this->connection)>0){
 						$bool = true;
-						$query = "UPDATE scommessa SET stato='1' WHERE idGara='$idGara' AND idCavallo='$idCavallo' AND nomeUtente='$username'";
+						$query = "UPDATE scommessa SET stato='1' WHERE idGara='$idGara' AND idCavallo='$idCavallo' AND nomeUtente='$username';";
 						mysqli_query($this->connection, $query);
 						
 						if(mysqli_affected_rows($this->connection)>0){
@@ -223,13 +223,13 @@
 						$bool = false;
 					}
 				}
-				$query = "UPDATE scommessa SET stato='1' WHERE idGara='$idGara' AND stato='0'";
+				$query = "UPDATE scommessa SET stato='1' WHERE idGara='$idGara' AND stato='0';";
 				mysqli_query($this->connection, $query);
 				if(mysqli_affected_rows($this->connection)>0)
 				$bool = true;
 				else $bool = false;
 				
-				$query = "UPDATE gara SET stato='2' where idGara='$idGara'";
+				$query = "UPDATE gara SET stato='2' where idGara='$idGara';";
 				mysqli_query($this->connection, $query);
 				if(mysqli_affected_rows($this->connection)>0){
 					$bool = true;
@@ -246,7 +246,7 @@
 		
 		public function evitaDoppioni($name)
 		{
-			$query = "select * from cavallo where nome='$name'";
+			$query = "select * from cavallo where nome='$name';";
 			
 			$result = mysqli_query($this->connection, $query);
 			
@@ -261,7 +261,7 @@
 		
 		public function eliminaCavallo($id)
 		{
-			$query = "update cavallo set ritiro='1' where idCavallo='$id'";
+			$query = "update cavallo set ritiro='1' where idCavallo='$id';";
 			mysqli_query($this->connection, $query);
 			if(mysqli_affected_rows($this->connection)>0){
 				return true;
@@ -277,28 +277,28 @@
 		
 	public function getCreditoUtente($username)
 	{
-		$query = "SELECT credito FROM utente WHERE nomeUtente='".$username."'";
+		$query = "SELECT credito FROM utente WHERE nomeUtente='".$username."';";
 		$result = mysqli_query($this->connection, $query);
 		return $result;
 	}
 	
 	public function getPosizioneCavalloScommessa($idGara,$idCavallo)
 	{
-		$query = "SELECT posizione FROM partecipante WHERE partecipante.idGara='".$idGara."' AND partecipante.idCavallo='".$idCavallo."'";
+		$query = "SELECT posizione FROM partecipante WHERE partecipante.idGara='".$idGara."' AND partecipante.idCavallo='".$idCavallo."';";
 		$result = mysqli_query($this->connection, $query);
 		return $result;
 	}
 	
 	public function getScommesseUtente($username)
 	{
-		$query = "SELECT DISTINCT scommessa.idGara,scommessa.idCavallo, dataGara, cavallo.nome, puntata, scommessa.stato, gara.stato AS statoGara FROM scommessa INNER JOIN partecipante ON scommessa.idGara = partecipante.idGara INNER JOIN cavallo ON scommessa.idCavallo = cavallo.idCavallo INNER JOIN gara ON partecipante.idGara = gara.idGara WHERE nomeUtente='".$username."'";
+		$query = "SELECT DISTINCT scommessa.idGara,scommessa.idCavallo, dataGara, cavallo.nome, puntata, scommessa.stato, gara.stato AS statoGara FROM scommessa INNER JOIN partecipante ON scommessa.idGara = partecipante.idGara INNER JOIN cavallo ON scommessa.idCavallo = cavallo.idCavallo INNER JOIN gara ON partecipante.idGara = gara.idGara WHERE nomeUtente='".$username."';";
 		$result = mysqli_query($this->connection, $query);
 		return $result;
 	}
 	
 	public function updateDopoPagamento($username, $costo)
 	{
-		$query = "UPDATE utente SET credito"."=credito-".$costo." WHERE nomeUtente='".$username."'";
+		$query = "UPDATE utente SET credito"."=credito-".$costo." WHERE nomeUtente='".$username."';";
 		if($this->connection->query($query))
 		{
 			return true;
@@ -312,7 +312,7 @@
 	
 	public function aggiuntaScommessa($username, $idGara, $idCavallo, $puntata)
 	{
-		$query = "INSERT INTO scommessa(idGara, idCavallo, nomeUtente, puntata) VALUES ('".$idGara."','".$idCavallo."','".$username."','".$puntata."')";
+		$query = "INSERT INTO scommessa(idGara, idCavallo, nomeUtente, puntata) VALUES ('".$idGara."','".$idCavallo."','".$username."','".$puntata."');";
 		if($this->connection->query($query))
 		{
 			return $this->updateDopoPagamento($username, $puntata);
@@ -326,15 +326,15 @@
 	public function confermaScommesse($idGara)
 	{
 		$noError = true;
-		$query = "SELECT * FROM scommessa WHERE idCavallo=(SELECT idCavallo FROM partecipante WHERE idGara=".$idGara." AND posizione=1) AND idGara=".$idGara." AND stato=0";
+		$query = "SELECT * FROM scommessa WHERE idCavallo=(SELECT idCavallo FROM partecipante WHERE idGara=".$idGara." AND posizione=1) AND idGara=".$idGara." AND stato=0;";
 		$resultQuery = mysqli_query($this->connection, $query);
 		while($row = mysqli_fetch_array($resultQuery))
 		{
 			$toAdd = $row['puntata']*2;
-			$queryUpdateCredito = "UPDATE utente SET credito"."=credito+".$toAdd." WHERE nomeUtente"." ='".$row['nomeUtente']."'";
+			$queryUpdateCredito = "UPDATE utente SET credito"."=credito+".$toAdd." WHERE nomeUtente"." ='".$row['nomeUtente']."';";
 			if($this->connection->query($queryUpdateCredito))
 			{
-				$queryUpdateStatoScommessa = "UPDATE scommessa SET stato=1 WHERE nomeUtente='".$row['nomeUtente']."' AND idGara=".$idGara;
+				$queryUpdateStatoScommessa = "UPDATE scommessa SET stato=1 WHERE nomeUtente='".$row['nomeUtente']."' AND idGara=".$idGara.";";
 				if($this->connection->query($queryUpdateStatoScommessa))
 				{
 					mysqli_free_result($resultQuery);
